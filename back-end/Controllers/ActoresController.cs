7813ec029a94d4cd9b8235aa4352928b5e6b2a03
Fernando Rocha Olivera <frochaolivera18@hotmail.com>
2021-04-07 23:04:39 -0500

    Added Actor entity and actor controller

---
+++ back-end/Controllers/ActoresController.cs
@@ -0,0 +1,32 @@
+﻿namespace back_end.Controllers
+{
+    using AutoMapper;
+    using back_end.DTOs;
+    using back_end.Entidades;
+    using Microsoft.AspNetCore.Mvc;
+    using System.Threading.Tasks;
+
+    [Route("api/actores")]
+    [ApiController]
+    public class ActoresController: ControllerBase
+    {
+        private readonly ApplicationDbContext context;
+        private readonly IMapper mapper;
+
+        public ActoresController(ApplicationDbContext context,
+            IMapper mapper)
+        {
+            this.context = context;
+            this.mapper = mapper;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post([FromBody] ActorCreacionDTO actorCreacionDTO)
+        {
+            var actor = mapper.Map<Actor>(actorCreacionDTO);
+            context.Add(actor);
+            await context.SaveChangesAsync();
+            return NoContent();
+        }
+    }
+}
